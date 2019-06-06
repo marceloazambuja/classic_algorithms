ec64e44e6e5949a80b58e736e4663c0c869c46a6
Marcelo Azambuja <marcelo.azambuja@gmail.com>
2019-06-06 17:16:05 -0300

    Sorting many arrays in Python

---
+++ pythonSolutions/sortingVector.py
@@ -0,0 +1,36 @@
+myArray = [30, 12, 40, 8, 50, 5]
+
+myArray.sort()
+
+print "Sorted array: "
+
+print myArray
+
+myArray.sort(reverse = True)
+
+print "Sorted descendent array: "
+
+print myArray
+
+print ("============= Ordering arrays with sublists:")
+
+secondArray = [(16,18), (14,15), (12,13), (8, 10), (13,1)]
+
+
+def orderFirst(val):
+    return(val[0])
+
+secondArray.sort(key=orderFirst)
+print secondArray
+
+
+def orderSecond(val):
+    return(val[1])
+
+print ("============= Ordering arrays with sublists by second value of sublist:")
+secondArray.sort(key=orderSecond)
+print secondArray
+
+print ("============= Ordering arrays with sublists by second value of sublist in descending order:")
+secondArray.sort(key=orderSecond, reverse=True)
+print secondArray
